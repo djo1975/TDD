ea15e8a0ab0c11d09848106f0ab5dd4cabf7afdb
Kigozi Joseph  Suubi <kigozijosephsuubi1@gmail.com>
2023-04-27 11:13:02 +0300

    Create solver.rb

---
+++ solver.rb
@@ -0,0 +1,24 @@
+class Solver
+  def factorial(number)
+    raise ArgumentError, 'Argument must be a non-negative integer' if !number.is_a?(Integer) || number.negative?
+    result = 1
+    (1..number).each do |i|
+      result *= i
+    end
+    result
+  end
+  def reverse(word)
+    word.reverse
+  end
+  def fizzbuzz(number, mock)
+    if mock.divisible_by_3?(number) && mock.divisible_by_5?(number)
+      'fizzbuzz'
+    elsif mock.divisible_by_3?(number)
+      'fizz'
+    elsif mock.divisible_by_5?(number)
+      'buzz'
+    else
+      number.to_s
+    end
+  end
+end
